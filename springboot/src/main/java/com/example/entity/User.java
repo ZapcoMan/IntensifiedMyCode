daf96cdabf14aa688e32c85bd7db1fe8a7f2790a
ZapcoMan <3507557934@qq.com>
2025-07-01 14:34:46 +0800

    refactor(springboot): 优化 User 类文档注释和代码结构
    - 在 User 类中添加了更多的文档注释，以描述每个字段和方法的作用
    - 优化了代码结构，使类定义更加清晰
    - 添加了用户 ID、用户名、密码、角色和姓名字段的注释
    - 更新了 token 和 avatar 方法的注释

--- springboot/src/main/java/com/example/entity/User.java
+++ springboot/src/main/java/com/example/entity/User.java
@@ -6,40 +6,64 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
- * 用户信息
+ * 用户信息类，继承自Account类
+ * 该类主要用于表示系统中的用户，包含用户的详细信息
  */
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class User extends Account {
+    // 用户ID
     private Integer id;
+    // 用户名
     private String username;
+    // 密码
     private String password;
+    // 角色
     private String role;
+    // 用户姓名
     private String name;
 
+    /**
+     * 获取用户令牌
+     *
+     * @return 用户令牌字符串
+     */
     @Override
     public String getToken() {
         return token;
     }
 
+    /**
+     * 设置用户令牌
+     *
+     * @param token 用户令牌字符串
+     */
     @Override
     public void setToken(String token) {
         this.token = token;
     }
 
+    // 用户令牌
     private String token;
 
+    // 用户头像URL
     private String avatar;
 
-
-
-
-
+    /**
+     * 获取用户头像URL
+     *
+     * @return 用户头像的URL字符串
+     */
     @Override
     public String getAvatar() {
         return avatar;
     }
 
+    /**
+     * 设置用户头像URL
+     *
+     * @param avatar 用户头像的URL字符串
+     */
     @Override
     public void setAvatar(String avatar) {
         this.avatar = avatar;
